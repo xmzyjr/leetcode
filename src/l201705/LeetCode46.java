2485f228f2326433fbc2be3c84ebffaf35bbc6b0
lanshan <yangjr@tuya.com>
2019-05-25 22:32:37 +0800

    lc 46

--- src/l201705/LeetCode46.java
+++ src/l201705/LeetCode46.java
@@ -1,10 +1,18 @@
 
 package l201705;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * @author lanshan
  */
 public class LeetCode46 {
+
     /**
      * 给定一个没有重复数字的序列，返回其所有可能的全排列。
      * 示例:
@@ -20,7 +28,30 @@ public class LeetCode46 {
      * ]
      */
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        get(nums, 0, lists);
+        return lists;
+    }
 
+    public void get(int[] nums, int index, List<List<Integer>> list) {
+        if (index == nums.length) {
+            List<Integer> collect = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            list.add(collect);
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            int temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+            get(nums, index + 1, list);
+            temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        LeetCode46 l = new LeetCode46();
+        System.out.println(l.permute(new int[] { 1, 2, 3 }));
     }
 }
-

04969116c9623af809098718d371595c75d9cfab
lanshan <yangjr@tuya.com>
2019-06-19 22:51:06 +0800

    73

---
+++ src/l201906/LeetCode73.java
@@ -0,0 +1,61 @@
+
+package l201906;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author lanshan
+ */
+public class LeetCode73 {
+
+    /**
+     * 给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+     * 示例 1:
+     * 输入:
+     * [
+     *   [1,1,1],
+     *   [1,0,1],
+     *   [1,1,1]
+     * ]
+     * 输出:
+     * [
+     *   [1,0,1],
+     *   [0,0,0],
+     *   [1,0,1]
+     * ]
+     * 示例 2:
+     * 输入:
+     * [
+     *   [0,1,2,0],
+     *   [3,4,5,2],
+     *   [1,3,1,5]
+     * ]
+     * 输出:
+     * [
+     *   [0,0,0,0],
+     *   [0,4,5,0],
+     *   [0,3,1,0]
+     * ]
+     */
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> x = new HashSet<>();
+        Set<Integer> y = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    x.add(i);
+                    y.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (x.contains(i) || y.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}

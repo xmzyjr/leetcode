f2c510657d4f681fb4e53ff3eb51101dc9f78b6e
lanshan <yangjr@tuya.com>
2019-07-07 02:32:09 +0800

    84

--- src/l201907/LeetCode84.java
+++ src/l201907/LeetCode84.java
@@ -1,6 +1,8 @@
 
 package l201907;
 
+import java.util.Stack;
+
 /**
  * @author lanshan
  */
@@ -35,5 +37,45 @@ public class LeetCode84 {
             max = sum > max ? sum : max;
         }
         return max;
+    }
+
+    public int largestRectangleArea2(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            while (i != 0 && stack.size() > 1 && heights[stack.peek()] >= heights[i]) {
+                max = Math.max(heights[stack.pop()]* (i - stack.peek() - 1), max);
+            }
+            stack.push(i);
+        }
+        Integer top = null;
+        if (stack.size() > 1) {
+            top = stack.peek();
+        }
+        while (stack.size() > 1) {
+            int now = stack.pop();
+            max = Math.max(max, (top - stack.peek()) * heights[now]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LeetCode84 l = new LeetCode84();
+        System.out.println(l.largestRectangleArea2(new int[] { 2, 1, 5, 6, 2, 3 }));
+    }
+
+    public int large(int[] heights) {
+        Stack < Integer > stack = new Stack < > ();
+        stack.push(-1);
+        int maxarea = 0;
+        for (int i = 0; i < heights.length; ++i) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+            stack.push(i);
+        }
+        while (stack.peek() != -1)
+            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
+        return maxarea;
     }
 }

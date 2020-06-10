34c5d0afa112226486efbeb77d4d3b8cd8c8f156
lanshan <yangjr@tuya.com>
2020-06-11 00:06:27 +0800

    102

---
+++ src/feture/tree/LeetCode102.java
@@ -0,0 +1,50 @@
+package feture.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lanshan
+ */
+public class LeetCode102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> re = new ArrayList<>();
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        if (root.left == null && root.right == null) {
+            List<Integer> result = new ArrayList<>();
+            result.add(root.val);
+            re.add(result);
+            return re;
+        }
+        List<TreeNode> treeNodeList = new ArrayList<>();
+        int start=0, end = 0;
+        treeNodeList.add(root);
+        end = 1;
+        while (start!=end) {
+            List<Integer> temp = new ArrayList<>();
+            for (int i = start; i < end; i++) {
+                TreeNode treeNode = treeNodeList.get(i);
+                temp.add(treeNode.val);
+                if (treeNode.left != null) {
+                    treeNodeList.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    treeNodeList.add(treeNode.right);
+                }
+            }
+            start = end;
+            end = treeNodeList.size();
+            re.add(temp);
+        }
+        return re;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}

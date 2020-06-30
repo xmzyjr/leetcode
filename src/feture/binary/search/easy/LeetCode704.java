ad325422eff6eab04de762db3e76f54b74ac4084
lanshan <yangjr@tuya.com>
2020-07-01 02:04:44 +0800

    704

---
+++ src/feture/binary/search/easy/LeetCode704.java
@@ -0,0 +1,25 @@
+package feture.binary.search.easy;
+
+/**
+ * @author lanshan
+ */
+public class LeetCode704 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            if (nums == null || nums.length == 0)
+                return -1;
+            int left = 0, right = nums.length - 1;
+            while (left<=right) {
+                int mid = left + (right-left)/2;
+                if (target == nums[mid]) {
+                    return mid;
+                } else if (target < nums[mid]) {
+                    right = mid - 1;
+                } else if (target > nums[mid]) {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}

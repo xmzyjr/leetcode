17b62316b7f6ae4813c359de397cd49209ce6054
lanshan <yangjr@tuya.com>
2020-07-19 20:02:11 +0800

    1227

---
+++ src/feture/dp/mid/LeetCode1227.java
@@ -0,0 +1,16 @@
+package feture.dp.mid;
+
+/**
+ * @author lanshan
+ */
+public class LeetCode1227 {
+    class Solution {
+        public double nthPersonGetsNthSeat(int n) {
+            if (n == 1) {
+                return 1;
+            } else {
+                return 1*1.0/2;
+            }
+        }
+    }
+}

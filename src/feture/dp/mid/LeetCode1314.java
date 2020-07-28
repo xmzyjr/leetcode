e986f01aac3a3d1e4369673b4815fe1e3a034165
lanshan <yangjr@tuya.com>
2020-07-29 01:27:43 +0800

    题目说明

--- src/feture/dp/mid/LeetCode1314.java
+++ src/feture/dp/mid/LeetCode1314.java
@@ -6,6 +6,26 @@ package feture.dp.mid;
  */
 public class LeetCode1314 {
 
+    /**
+     * 给你一个 m * n 的矩阵 mat 和一个整数 K ，请你返回一个矩阵 answer ，其中每个 answer[i][j] 是所有满足下述条件的元素 mat[r][c] 的和： 
+     *
+     * i - K <= r <= i + K, j - K <= c <= j + K 
+     * (r, c) 在矩阵内。
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：mat = [[1,2,3],[4,5,6],[7,8,9]], K = 1
+     * 输出：[[12,21,16],[27,45,33],[24,39,28]]
+     * 示例 2：
+     *
+     * 输入：mat = [[1,2,3],[4,5,6],[7,8,9]], K = 2
+     * 输出：[[45,45,45],[45,45,45],[45,45,45]]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/matrix-block-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
     class Solution {
 
         public int[][] matrixBlockSum(int[][] mat, int K) {

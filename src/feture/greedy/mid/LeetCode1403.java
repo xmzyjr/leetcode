18dd8ac8a538d44b4e51793fb1f696956d24e550
lanshan <yangjr@tuya.com>
2020-09-28 01:24:21 +0800

    1403

---
+++ src/feture/greedy/mid/LeetCode1403.java
@@ -0,0 +1,36 @@
+package feture.greedy.mid;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author lanshan
+ */
+public class LeetCode1403 {
+
+    public List<Integer> minSubsequence(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return new ArrayList<>();
+        if (nums.length == 1)
+            return Collections.singletonList(nums[0]);
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        double half = sum * 1.0 / 2;
+        Arrays.sort(nums);
+        sum = 0;
+        List<Integer> list = new ArrayList<>(nums.length / 2 + 1);
+        for (int i = nums.length-1; i >= 0; i--) {
+            int num = nums[i];
+            sum += num;
+            list.add(num);
+            if (sum >= half) {
+                break;
+            }
+        }
+        return list;
+    }
+}

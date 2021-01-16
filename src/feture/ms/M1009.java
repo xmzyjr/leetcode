5dcfff6ae014a3e130abf115a4885b8047b628a3
lanshan <yangjr@tuya.com>
2021-01-17 01:34:32 +0800

    1009

--- src/feture/ms/M1009.java
+++ src/feture/ms/M1009.java
@@ -1,4 +1,41 @@
-package feture.ms;/** 
+package feture.ms;
+
+/**
  * @author lanshan
- */public class M1009 {
+ */
+public class M1009 {
+    /**
+     * 给定M×N矩阵，每一行、每一列都按升序排列，请编写代码找出某元素。
+     *
+     * 示例:
+     *
+     * 现有矩阵 matrix 如下：
+     *
+     * [
+     *   [1,   4,  7, 11, 15],
+     *   [2,   5,  8, 12, 19],
+     *   [3,   6,  9, 16, 22],
+     *   [10, 13, 14, 17, 24],
+     *   [18, 21, 23, 26, 30]
+     * ]
+     * 给定 target = 5，返回 true。
+     *
+     * 给定 target = 20，返回 false。
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0)
+            return false;
+        int x = 0;
+        int y = matrix[0].length - 1;
+        while (x < matrix.length && y >= 0) {
+            if (matrix[x][y] < target) {
+                x++;
+            } else if (matrix[x][y] > target) {
+                y--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }

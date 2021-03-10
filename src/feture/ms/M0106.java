4eee56cf06c02a8c3691e4f476b66d9be9d8b585
lanshan <yangjr@tuya.com>
2021-03-10 23:27:43 +0800

    t

---
+++ src/feture/ms/M0106.java
@@ -0,0 +1,53 @@
+package feture.ms;
+
+/**
+ * @author lanshan
+ */
+public class M0106 {
+    /**
+     * 字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。
+     * 若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。
+     *
+     * 示例1:
+     *
+     *  输入："aabcccccaaa"
+     *  输出："a2b1c5a3"
+     * 示例2:
+     *
+     *  输入："abbccd"
+     *  输出："abbccd"
+     *  解释："abbccd"压缩后为"a1b2c2d1"，比原字符串长度更长。
+     * 提示：
+     *
+     * 字符串长度在[0, 50000]范围内。
+     */
+    public String compressString(String S) {
+        if (S == null || S.length() == 0) {
+            return S;
+        }
+        StringBuilder sb = new StringBuilder();
+        char[] chars = S.toCharArray();
+        int count = 1;
+        char pre = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == pre) {
+                count++;
+            } else {
+                sb.append(pre).append(count);
+                pre = chars[i];
+                count = 1;
+            }
+        }
+        if (count != 0) {
+            sb.append(pre).append(count);
+        }
+        return sb.length() < S.length() ? sb.toString() : S;
+    }
+
+    public static void main(String[] args) {
+        M0106 m = new M0106();
+        System.out.println(m.compressString("aabcccccaaa"));
+        System.out.println(m.compressString("abbccd"));
+        System.out.println(m.compressString("ab"));
+    }
+}

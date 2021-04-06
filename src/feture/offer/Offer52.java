8807c16cd354768a8609436f0a49c8e3e84379cd
蓝山(杨家仁) <yangjr@tuya.com>
2021-04-06 12:12:46 +0800

    Offer52

---
+++ src/feture/offer/Offer52.java
@@ -0,0 +1,68 @@
+package feture.offer;
+
+/**
+ * @author lanshan
+ */
+public class Offer52 {
+
+    /**
+     * 输入两个链表，找出它们的第一个公共节点。
+     *
+     * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+     * 输出：Reference of the node with value = 8
+     * 输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+     *  
+     *
+     * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+     * 输出：Reference of the node with value = 2
+     * 输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+     *
+     * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+     * 输出：null
+     * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+     * 解释：这两个链表不相交，因此返回 null。
+     *  
+     *
+     * 注意：
+     *
+     * 如果两个链表没有交点，返回 null.
+     * 在返回结果后，两个链表仍须保持原有的结构。
+     * 可假定整个链表结构中没有循环。
+     * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        boolean change1 = false, change2 = false;
+        while (temp1 != null || temp2 != null) {
+            if (!change1 && temp1 == null) {
+                change1 = true;
+                temp1 = headB;
+            } else if (temp1 == null) {
+                return null;
+            }
+            if (!change2 && temp2 == null) {
+                change2 = true;
+                temp2 = headA;
+            } else if (temp2 == null) {
+                return null;
+            }
+            if (temp1 == temp2)
+                return temp1;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return null;
+    }
+}
